9b3a19379e9cb4db16a9ad6f6cfc4ecf2aca08aa
inegmetov <negmetov.i@gmail.com>
2022-10-17 11:55:22 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [count, setCount] =  useState(0)
+
+  // return <h1>Hello React!!!</h1>;
+  // return e('h1', {},'Hello from js')
+  return ()
 }
 
 export default App;
