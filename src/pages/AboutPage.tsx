1b36db6aa5071b310f9160a87e6fede9aa676d0d
inegmetov <negmetov.i@gmail.com>
2022-10-23 17:14:04 +0300

    routing navigation

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores porro quasi tempore voluptates nisi dolorum. Animi rerum quo maxime doloribus?
+        </div>
+    );
+}
+
+export default AboutPage;
